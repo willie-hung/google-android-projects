dfd064807ee04156849af01937fb46fd28608e10
Willie Hung <willie880201044@gmail.com>
2024-05-04 14:55:08 -0500

    Add projects
    Signed-off-by: Willie Hung <willie880201044@gmail.com>

--- Superheroes/app/build.gradle.kts
+++ Superheroes/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.woof"
+    namespace = "com.example.superheroes"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.woof"
+        applicationId = "com.example.superheroes"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -59,7 +59,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("androidx.compose.material:material-icons-extended")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
